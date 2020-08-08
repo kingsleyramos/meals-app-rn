f9ee3fd664232fd6cbd86308e5a2138d82738dc6
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-08-08 14:20:38 -0700

    Created categoty meals screen

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const CategoriesScreen = (props) => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default CategoriesScreen
+
+const styles = StyleSheet.create({})
