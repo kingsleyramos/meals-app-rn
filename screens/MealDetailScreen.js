171e9510f0be982c4a539a3d3ba3fa71b5cf293a
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-08-08 14:20:52 -0700

    Created new screens

---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const MealDetailScreen = (props) => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default MealDetailScreen
+
+const styles = StyleSheet.create({})
