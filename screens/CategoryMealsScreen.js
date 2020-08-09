d47c77753fdc2ebe1ec283d0f3296cb9047d9947
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-08-08 18:06:51 -0700

    Added buttons and removed changed to alignItems

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,21 +1,35 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
 const CategoryMealsScreen = (props) => {
     return (
         <View style={styles.screen}>
             <Text>The Category Meal Screen</Text>
+            <Button
+                title='Go to Meal Details!'
+                onPress={() => {
+                    props.navigation.navigate('MealDetail');
+                    // props.navigation.push('MealDetail'); // if you want to navigate to the same screen
+                }}
+            />
+            <Button
+                title='Go Back'
+                onPress={() => {
+                    props.navigation.goBack(); // this can be used on other navigators
+                    // props.navigation.pop(); // can be only be used on stack navigators
+                }}
+            />
         </View>
-    )
-}
+    );
+};
 
-export default CategoryMealsScreen
+export default CategoryMealsScreen;
 
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
         justifyContent: 'center',
-        alignContent: 'center'
-    }
-})
-
+        alignItems: 'center',
+    },
+});
+Î;
